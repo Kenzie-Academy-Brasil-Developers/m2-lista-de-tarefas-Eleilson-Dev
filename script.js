b7c9fe76727a58ffe1a8f467238a97fc81363bb7
Eleilson-Dev <maxta.gamer@gmail.com>
2024-01-20 21:51:57 -0300

    projeto finalizado update

--- script.js
+++ script.js
@@ -4,16 +4,16 @@ const titleTask = form.querySelector('#input_title');
 const typeTask = form.querySelector('.form__input--priority');
 
 const tasks = [
-  // { title: 'Comprar comida para o gato', type: 'Urgente' },
-  // { title: 'Consertar Computador', type: 'Importante' },
-  // { title: 'Beber água', type: 'Normal' },
-  // { title: 'Enviar relatório trimestral', type: 'Importante' },
-  // { title: 'Fazer exercícios físicos', type: 'Normal' },
-  // { title: 'Agendar consulta médica', type: 'Urgente' },
-  // { title: 'Ler pelo menos um capítulo de um livro', type: 'Normal' },
-  // { title: 'Limpar a despensa', type: 'Importante' },
-  // { title: 'Pagar a conta de energia', type: 'Urgente' },
-  // { title: 'Assistir a um documentário interessante', type: 'Normal' },
+  { title: 'Comprar comida para o gato', type: 'Urgente' },
+  { title: 'Consertar Computador', type: 'Importante' },
+  { title: 'Beber água', type: 'Normal' },
+  { title: 'Enviar relatório trimestral', type: 'Importante' },
+  { title: 'Fazer exercícios físicos', type: 'Normal' },
+  { title: 'Agendar consulta médica', type: 'Urgente' },
+  { title: 'Ler pelo menos um capítulo de um livro', type: 'Normal' },
+  { title: 'Limpar a despensa', type: 'Importante' },
+  { title: 'Pagar a conta de energia', type: 'Urgente' },
+  { title: 'Assistir a um documentário interessante', type: 'Normal' },
 ];
 
 // funcao resposavel por criar elementos DOM
